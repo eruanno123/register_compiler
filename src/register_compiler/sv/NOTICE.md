8a060a5f309036ba66e3171133eb9c1251258160
eruanno123 <3846502+eruanno123@users.noreply.github.com>
2018-11-12 11:33:36 +0100

    incorporate parts of pyverilog package

---
+++ src/register_compiler/sv/NOTICE.md
@@ -0,0 +1,12 @@
+Notice
+======
+
+This directory is a derivative work based on pyverilog by Shinya Takamaeda-Yamazaki.
+The original license information can be found here: https://github.com/PyHDI/Pyverilog
+
+List of changes
+---------------
+
+- Include code generation only related classes
+- Add some SystemVerilog specific constructs
+- Keep Linter calm
--- tests/test_rc.py
+++ tests/test_rc.py
@@ -3,8 +3,14 @@ import pytest
 from systemrdl import RDLCompileError
 from register_compiler.rc import MyCompiler
 
+from register_compiler.sv.codegen import ASTCodeGenerator
+
 
 def test_nonExistingFile():
     with pytest.raises(FileNotFoundError):
         compiler = MyCompiler(["non_existing_file"])
         compiler.compile()
+
+
+def test_astGenerator():
+    cgen = ASTCodeGenerator()
